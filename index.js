97df07a31646c707e8370c9751215f94bd9c8037
THINESHKUMAR <thinesh12@gmal.com>
2023-03-25 19:50:15 -0400

    edit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function razzle(){
+    console.log("You've been razzled");
+}
+razzle();
+
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`;
+}
+function wrapAdjective(wrap="*"){
+    return function(Adjective){
+        return `You are ${wrap}${Adjective}${wrap}!`
+}}
